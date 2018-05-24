25616ccf0e86e9abe00d1def9842c8e81c66d3a9
Robin Green <greenrj4@gmail.com>
2018-05-24 17:18:58 -0600

    edits, destroy

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -22,6 +22,27 @@ class TodosController < ApplicationController
     end
   end
 
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+
+      if @todo.update(todos_params)
+        redirect_to todos_path
+      else
+        render :edit
+        end
+      end
+
+      def destroy
+        Todo.find(params[:id]).destroy
+        redirect_to todos_path
+      end
+
+
+
   private
     def todos_params
       params.require(:todo).permit(:name, :importance)
